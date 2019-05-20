a8a0817784079d6c925784ef48d5cc0cc41ab921
Mario Amaya <thefozcat@gmail.com>
2019-05-20 11:45:08 -0700

    updated gitignore and port on index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const envReader = require('dotenv');
 const server = require('./api/server');
 
-const port =  5000;
+envReader.config();
+
+const port =  process.env.PORT || 5000;
 
 server.listen(port, () => {
     console.log(`\n *** Listening on http://localhost:${port} *** \n`);
