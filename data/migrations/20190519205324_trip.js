c45ebb712bb43a85b4de6da96d80f34558701503
Mario Amaya <thefozcat@gmail.com>
2019-05-19 20:59:23 -0700

    created migrations

---
+++ data/migrations/20190519205324_trip.js
@@ -0,0 +1,26 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('trip', tbl => {
+        tbl.increments();
+        tbl
+            .text('item', 128)
+            .notNullable();
+        tbl
+            .text('description');
+        tbl
+            .integer('priority');
+        tbl
+            .date('due_date');
+        tbl
+            .integer('user_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE');
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('trip');
+};
