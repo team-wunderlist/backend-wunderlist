c592d44329efe78d917de7f5d91ca5885b4fb18b
Mario Amaya <thefozcat@gmail.com>
2019-05-21 12:19:12 -0700

    updated web token expiresIn time

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -56,7 +56,7 @@ function generateToken(user) {
         username: user.username
     }
     const options = {
-        expiresIn: '1hr'
+        expiresIn: '24hr'
     }
     return jwt.sign(payload, secrets.jwtSecret, options)
 }
